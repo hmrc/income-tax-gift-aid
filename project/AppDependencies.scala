12b06c06793751322c50ae26c89abceeb0ad7f5d
michael-robert-poole <81170372+michael-rob-poole@users.noreply.github.com>
2021-06-02 16:18:57 +0100

    update scalatest-plus

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -30,7 +30,7 @@ object AppDependencies {
     "com.typesafe.play"       %% "play-test"                % current  % "test, it",
     "org.pegdown"             %  "pegdown"                  % "1.6.0"  % "test, it",
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"  % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0"  % "test, it",
     "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.28.0" % "test, it",
     "org.scalamock"           %% "scalamock"                % "5.1.0"  % "test, it"
   )
