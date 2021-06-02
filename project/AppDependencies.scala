4cb676f521f61ada25fcb8ad99940f6f4e3a07dc
michael-robert-poole <81170372+michael-rob-poole@users.noreply.github.com>
2021-06-02 14:02:30 +0100

    add vladsch

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,19 +19,19 @@ import sbt._
 
 object AppDependencies {
 
-  val compile: Seq[ModuleID] = Seq(
+  val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "5.3.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.2"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"  % "2.12.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.3.0",
-    "org.scalatest"           %% "scalatest"                % "3.2.9",
-    "org.jsoup"               %  "jsoup"                    % "1.13.1",
-    "com.typesafe.play"       %% "play-test"                % current,
-    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0",
-    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.28.0",
-    "org.scalamock"           %% "scalamock"                % "5.1.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % "0.50.0"
-  ).map(_ % "test, it")
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.3.0" % "test, it",
+    "org.scalatest"           %% "scalatest"                % "3.2.9"  % "test, it",
+    "com.typesafe.play"       %% "play-test"                % current  % "test, it",
+    "org.pegdown"             %  "pegdown"                  % "1.6.0"  % "test, it",
+    "com.vladsch.flexmark"    %  "flexmark-all"             % "0.36.8" % "test, it",
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "4.0.3"  % "test, it",
+    "com.github.tomakehurst"  %  "wiremock-jre8"            % "2.28.0" % "test, it",
+    "org.scalamock"           %% "scalamock"                % "5.1.0"  % "test, it"
+  )
 }
