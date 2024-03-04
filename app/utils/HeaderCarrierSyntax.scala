00a1e37aa4279c142096c82633dc0e5ec7dc8cf8
basotia <16934889+basotia@users.noreply.github.com>
2024-03-04 14:15:17 +0000

    Copyright changes

--- app/utils/HeaderCarrierSyntax.scala
+++ app/utils/HeaderCarrierSyntax.scala
@@ -1,5 +1,5 @@
 /*
- * Copyright 2023 HM Revenue & Customs
+ * Copyright 2024 HM Revenue & Customs
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package utils
 
+import models.logging.CorrelationId
 import uk.gov.hmrc.http.{HeaderCarrier, HeaderNames}
 
 object HeaderCarrierSyntax {
@@ -24,20 +25,22 @@ object HeaderCarrierSyntax {
 
     def addExplicitHeaders :Seq[(String,String)] ={
       Seq(
-        HeaderNames.xRequestId            -> hc.requestId.map(_.value),
-        HeaderNames.xSessionId            -> hc.sessionId.map(_.value),
-        HeaderNames.xForwardedFor         -> hc.forwarded.map(_.value),
-        HeaderNames.xRequestChain         -> Some(hc.requestChain.value),
-        HeaderNames.authorisation         -> hc.authorization.map(_.value),
-        HeaderNames.trueClientIp          -> hc.trueClientIp,
-        HeaderNames.trueClientPort        -> hc.trueClientPort,
-        HeaderNames.googleAnalyticTokenId -> hc.gaToken,
-        HeaderNames.googleAnalyticUserId  -> hc.gaUserId,
-        HeaderNames.deviceID              -> hc.deviceID,
-        HeaderNames.akamaiReputation      -> hc.akamaiReputation.map(_.value)
+        HeaderNames.xRequestId                -> hc.requestId.map(_.value),
+        HeaderNames.xSessionId                -> hc.sessionId.map(_.value),
+        HeaderNames.xForwardedFor             -> hc.forwarded.map(_.value),
+        HeaderNames.xRequestChain             -> Some(hc.requestChain.value),
+        HeaderNames.authorisation             -> hc.authorization.map(_.value),
+        HeaderNames.trueClientIp              -> hc.trueClientIp,
+        HeaderNames.trueClientPort            -> hc.trueClientPort,
+        HeaderNames.googleAnalyticTokenId     -> hc.gaToken,
+        HeaderNames.googleAnalyticUserId      -> hc.gaUserId,
+        HeaderNames.deviceID                  -> hc.deviceID,
+        HeaderNames.akamaiReputation          -> hc.akamaiReputation.map(_.value),
+        CorrelationId.CorrelationIdHeaderKey  -> maybeCorrelationId
       ).collect { case (k, Some(v)) => (k, v) }
     }
 
+    def maybeCorrelationId: Option[String] = hc.headers(List(CorrelationId.CorrelationIdHeaderKey)).map(_._2).headOption
 
   }
 
